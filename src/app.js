58242f885721ad7265e5009290d7ac6bf50ee467
scino11 <prod.scino@gmail.com>
2024-09-02 15:26:03 -0300

    styles register-login

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-import express, { json } from 'express'
+import express from 'express'
 import clientRouter from './router/cliente.router.js'
 import bookingRouter from './router/reserva.router.js'
 import registerRouter from './router/register.router.js'
 const app = express()
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));
